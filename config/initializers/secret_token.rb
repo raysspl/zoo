42fa978ee3abf47cff7fd78497cebae5731242cf
Tim Preston <tim@tp.id.au>
2013-08-26 12:28:51 -0700

    Initial commit with postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zoo::Application.config.secret_key_base = '57c0006815b5f485fa81ad177d186fe20594797c23a8b5dc23338f9135c4404ffa5f3646d2ae47766b6419e6ebfe1b0fafdc0f0990880535f4c8a4ab0c301016'
